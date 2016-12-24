4f6728a1008f456848aa24ea114386898f017485
Insan Ganang Putranda <insan.putranda@gmail.com>
2016-12-24 21:33:46 +0700

    Added abaddon full skill AI

--- ability_item_usage_abaddon.lua
+++ ability_item_usage_abaddon.lua
@@ -25,42 +25,170 @@ local tableAbilitiesToLearn = {
   "special_bonus_unique_abaddon"
 }
 
+pcall(require, "bots/utility/ability_usage" )
+
+----------------------------------------------------------------------------------------------------
+
+local castDCDesire = 0;
+local castASDesire = 0;
+local castBTDesire = 0;
+local abilityDC = nil;
+local abilityAS = nil;
+local abilityBT = nil;
+local nBTThreshold = nil;
+local dModifier = 0.0;
+
+
 function AbilityUsageThink()
   local npcBot = GetBot();
   local loc = npcBot:GetLocation();
+  abilityDC = npcBot:GetAbilityByName( "abaddon_death_coil" );
+  abilityAS = npcBot:GetAbilityByName( "abaddon_aphotic_shield" );
+  abilityBT = npcBot:GetAbilityByName( "abaddon_borrowed_time" );
+  nBTThreshold = abilityBT:GetSpecialValueInt( "hp_threshold" );
+  if ( nBTThreshold == 0) then
+    nBTThreshold = 0.5*npcBot:GetMaxHealth();
+  end;
 
   abilityLevelUp( npcBot, tableAbilitiesToLearn );
+
+  -- Check if we're already using an ability
+	if ( npcBot:IsUsingAbility() ) then return end;
+
+  -- Consider using each ability
+	castDCDesire, castDCTarget = ConsiderDeathCoil( npcBot );
+	castASDesire, castASTarget = ConsiderAphoticShield( npcBot );
+	castBTDesire = ConsiderBorrowedTime( npcBot );
+
+  if ( castBTDesire > BOT_ACTION_DESIRE_NONE )
+	then
+		npcBot:Action_UseAbility( abilityBT );
+		return;
+	end
+
+	if ( castDCDesire > BOT_ACTION_DESIRE_NONE )
+	then
+		npcBot:Action_UseAbilityOnEntity( abilityDC, castDCTarget );
+		return;
+	end
+
+	if ( castASDesire > BOT_ACTION_DESIRE_NONE )
+	then
+		npcBot:Action_UseAbilityOnEntity( abilityAS, castASTarget );
+		return;
+	end
+
 end
 
-function abilityLevelUp( npcBot, tableAbilitiesToLearn )
-  if ( tableAbilitiesToLearn == 0 )
-  then
-    return;
+----------------------------------------------------------------------------------------------------
+
+function CanCastDeathCoilOnTarget( npcTarget )
+	return npcTarget:CanBeSeen() and not npcTarget:IsMagicImmune() and not npcTarget:IsInvulnerable();
+end
+
+
+function CanCastAphoticShieldOnTarget( npcTarget )
+	return npcTarget:CanBeSeen();
+end
+
+----------------------------------------------------------------------------------------------------
+
+function ConsiderDeathCoil( npcBot )
+  if ( not abilityDC:IsFullyCastable() )
+	then
+		return BOT_ACTION_DESIRE_NONE, 0;
+	end;
+
+  -- Get some of its values
+	local nCastRange = abilityDC:GetCastRange();
+	local nSelfDamage = abilityDC:GetSpecialValueInt( "self_damage" );
+  local nTargetDamage = abilityDC:GetSpecialValueInt( "target_damage" );
+  local nHealAmount = abilityDC:GetSpecialValueInt( "heal_amount" );
+
+  local botMaxHealth = npcBot:GetMaxHealth();
+  local botCurrentHealth = npcBot:GetHealth();
+
+  if ( abilityBT:IsActivated() ) then
+    dModifier = 0.25;
   end
 
-  local sNextAbility = tableAbilitiesToLearn[1];
+  -- regular death coil usage
+  if ( ( botCurrentHealth - nSelfDamage ) > nBTThreshold ) then
+    -- decide to damage current enemy target?
+    local npcTarget = npcBot:GetTarget();
 
-  if (sNextAbility == nil) then
-    return;
-  end
+		if ( npcTarget ~= nil ) then
+			if ( CanCastDeathCoilOnTarget( npcTarget )
+          and GetUnitToUnitDistance( npcBot, npcTarget ) > 200 ) then
 
-  -- skip talent abilites, still not working
-  if ( string.find( sNextAbility, "special") and tableAbilitiesToLearn ~= nil ) then
-    table.remove( tableAbilitiesToLearn, 1 );
-    return;
-  end
+				return BOT_ACTION_DESIRE_MODERATE + dModifier, npcTarget;
+			end
+		end
 
-  ability = npcBot:GetAbilityByName( sNextAbility );
-  local aLevel = ability:GetLevel();
-  if ( ability:CanAbilityBeUpgraded() ) then
-    npcBot:Action_LevelAbility( sNextAbility );
-    local aLevelResult = ability:GetLevel();
+    -- decide to heal nearby ally?
+    local tableNearbyAllyHeroes = npcBot:GetNearbyHeroes( nCastRange, false, BOT_MODE_NONE );
+    for _,npcAlly in pairs( tableNearbyAllyHeroes ) do
+      if ( npcAlly:GetUnitName() ~= 'npc_dota_hero_abaddon' ) then
+        local allyMaxHealth = npcAlly:GetMaxHealth();
+        local allyCurrentHealth = npcAlly:GetHealth();
 
-    if ( aLevelResult > aLevel and tableAbilitiesToLearn ~= nil ) then
-      print("learned " .. sNextAbility );
-      table.remove( tableAbilitiesToLearn, 1 );
+        if ( allyCurrentHealth < 0.5*allyMaxHealth ) then
+          if ( CanCastDeathCoilOnTarget( npcAlly ) ) then
+    				return BOT_ACTION_DESIRE_MODERATE + dModifier, npcAlly;
+    			end
+        end
+      end
     end
   end
 
-  return;
+  -- ultimate is active, spam death coil to target
+  if ( abilityBT:IsActivated() ) then
+    local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes( nCastRange, true, BOT_MODE_NONE );
+    for _,npcEnemy in pairs( tableNearbyEnemyHeroes ) do
+      if ( CanCastDeathCoilOnTarget( npcEnemy ) ) then
+        return BOT_ACTION_DESIRE_MODERATE + dModifier, npcEnemy;
+      end
+    end
+  end
+
+  return BOT_ACTION_DESIRE_NONE, 0;
+end
+
+function ConsiderAphoticShield( npcBot )
+  if ( not abilityAS:IsFullyCastable() )
+	then
+		return BOT_ACTION_DESIRE_NONE, 0;
+	end;
+
+  if ( abilityBT:IsActivated() ) then
+    dModifier = 0.25;
+  end
+
+  local nCastRange = abilityAS:GetCastRange();
+
+  local tableNearbyAllyHeroes = npcBot:GetNearbyHeroes( nCastRange, false, BOT_MODE_NONE );
+  for _,npcAlly in pairs( tableNearbyAllyHeroes ) do
+    if ( CanCastAphoticShieldOnTarget( npcAlly ) ) then
+      -- cover recently damaged ally
+      if ( npcAlly:WasRecentlyDamagedByAnyHero(1.0) ) then
+        return BOT_ACTION_DESIRE_MODERATE + dModifier, npcAlly;
+      elseif ( npcAlly:IsBlind() or npcAlly:IsChanneling()
+              or npcAlly:IsDisarmed() or npcAlly:IsHexed()
+              or npcAlly:IsRooted() or npcAlly:IsMuted()
+              or npcAlly:IsSilenced() or npcAlly:IsStunned()) then
+        return BOT_ACTION_DESIRE_HIGH + dModifier, npcAlly;
+      end
+    end
+  end
+
+  return BOT_ACTION_DESIRE_NONE, 0;
+end
+
+function ConsiderBorrowedTime( npcBot )
+  if ( not abilityBT:IsFullyCastable() )
+	then
+		return BOT_ACTION_DESIRE_NONE;
+	end;
+
+  return BOT_ACTION_DESIRE_NONE, 0;
 end
---
+++ utility/ability_usage.lua
@@ -0,0 +1,32 @@
+function abilityLevelUp( npcBot, tableAbilitiesToLearn )
+  if ( tableAbilitiesToLearn == 0 )
+  then
+    return;
+  end
+
+  local sNextAbility = tableAbilitiesToLearn[1];
+
+  if (sNextAbility == nil) then
+    return;
+  end
+
+  -- skip talent abilites, still not working
+  if ( string.find( sNextAbility, "special") and tableAbilitiesToLearn ~= nil ) then
+    table.remove( tableAbilitiesToLearn, 1 );
+    return;
+  end
+
+  ability = npcBot:GetAbilityByName( sNextAbility );
+  local aLevel = ability:GetLevel();
+  if ( ability:CanAbilityBeUpgraded() ) then
+    npcBot:Action_LevelAbility( sNextAbility );
+    local aLevelResult = ability:GetLevel();
+
+    if ( aLevelResult > aLevel and tableAbilitiesToLearn ~= nil ) then
+      print("learned " .. sNextAbility );
+      table.remove( tableAbilitiesToLearn, 1 );
+    end
+  end
+
+  return;
+end
